61ec58c3bdc51ad014c5763fd4a0d046dd76b533
Nathan Knowler <nathan@knowler.me>
2019-06-11 00:26:18 -0600

    Reimplement aliasing

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import { Global } from '@emotion/core'
 import { ThemeProvider } from 'emotion-theming'
-import { globalStyles, theme } from '../theme'
+import { globalStyles, theme } from 'theme'
 
 const query = graphql`
   query SiteTitleQuery {
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,1 @@
-export { Layout } from './Layout.js'
+export { Layout } from './Layout'
